b8fdf1054f973504e030ffefcf25fa85c28db2ae
pomelo <wangmenglei@benditoutiao.com>
2018-09-20 02:10:43 +0800

    testng含失败用例

---
+++ src/main/java/com/job/ui/contentmanager/con3.java
@@ -0,0 +1,16 @@
+package com.job.ui.contentmanager;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class con3 {
+	
+	@Test
+	public void ys() {
+		int code = 1;
+		int icode = 2;
+	
+	      Assert.assertEquals(code,icode);
+	}
+
+}
